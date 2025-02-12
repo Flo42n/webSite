cd960a604666acd4897822cc6afb00f6ac97c6fc
Océane Delogé <oceanedeloge@Oceanes-MacBook-Air.local>
2025-02-12 11:24:10 +0100

    debut page our team

--- app/our_team/page.tsx
+++ app/our_team/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
@@ -8,6 +9,19 @@ export default function our_team() {
   return (
     <div className="bg-[#FAEBD7] pt-2">
       <Header />
+      <div className="grid grid-rows items-center justify-items-center min-h-screen font-mono  gap-10  bg-[#FAEBD7] text-black  p-5">
+        <Image 
+          src="/joynestLogo.jpg" 
+          alt="Our Team Image" 
+          width={600} 
+          height={600} 
+          className="rounded-lg shadow-lg" 
+        />
+        <p className="text-center text-2xl pt-1">
+          Discover the talented individuals who bring
+          <br /> creativity and passion to our captivating world.
+        </p>
+      </div>
       <Footer />
     </div>
   );
