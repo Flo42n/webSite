fef075f31ab8289204ed7aeca050b54d807a0f83
Floralie <floralie.niort@gmail.com>
2025-02-12 13:14:59 +0100

    probleme our team

--- app/our_team/page.tsx
+++ app/our_team/page.tsx
@@ -9,15 +9,15 @@ export default function our_team() {
   return (
     <div className="bg-[#FAEBD7] pt-2">
       <Header />
-      <div className="grid grid-rows items-center justify-items-center min-h-screen font-mono  gap-10  bg-[#FAEBD7] text-black  p-5">
-        <Image 
-          src="/joynestLogo.jpg" 
-          alt="Our Team Image" 
-          width={600} 
-          height={600} 
-          className="rounded-lg shadow-lg" 
+      <div className="grid grid-rows items-center justify-items-center font-mono  gap-5  bg-[#FAEBD7] text-black  p-5">
+        <Image
+          src="/joynestLogo.png"
+          alt="Our Team Image"
+          width={200}
+          height={200}
+          className=" w-2/3 m-16 "
         />
-        <p className="text-center text-2xl pt-1">
+        <p className="text-center text-2xl">
           Discover the talented individuals who bring
           <br /> creativity and passion to our captivating world.
         </p>
