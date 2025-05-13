7e04e73c74ca41497e112cdbd469e3cfd4179722
Océane Delogé <oceanedeloge@Oceanes-MacBook-Air.local>
2025-05-13 14:06:12 +0200

    fonts et downlaod links

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 
-interface CardProps {
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
   className?: string;
 }
 
-export function Card({ children, className = "" }: CardProps) {
-  return <div className={`bg-[#e4d4be] shadow-md rounded-lg p-7 ${className}`}>{children}</div>;
+export function Card({ children, className = "", style, ...rest }: CardProps) {
+  return <div className={`bg-[#e4d4be] shadow-md rounded-lg p-7 ${className}`}
+    style={style}
+    {...rest}
+    >{children}
+  </div>;
 }
 
 export function CardContent({ children }: { children: React.ReactNode }) {
